0f29f0fdfe46e9f7c0e47318c2243a83f0ef637d
Melondica <37456374+Melondica@users.noreply.github.com>
2018-03-16 13:33:21 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,31 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-
-client.on('ready', () => {
-    console.log('I am ready!');
-});
-
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
-});
+{
+  "ClientId": 424287787139989504,
+  "Token": "NDI0Mjg3Nzg3MTM5OTg5NTA0.DY2sZg.fm5UZMp-uEgSY-FCBdx2irRjck4",
+  "OwnerIds": [
+    0
+  ],
+  "LoLApiKey": "",
+  "GoogleApiKey": "",
+  "MashapeKey": "",
+  "OsuApiKey": "",
+  "SoundCloudClientId": "",
+  "CleverbotApiKey": "",
+  "CarbonKey": "",
+  "Db": {
+    "Type": "sqlite",
+    "ConnectionString": "Data Source=data/NadekoBot.db"
+  },
+  "TotalShards": 1,
+  "PatreonAccessToken": "",
+  "PatreonCampaignId": "334038",
+  "RestartCommand": null,
+  "ShardRunCommand": "",
+  "ShardRunArguments": "",
+  "ShardRunPort": null,
+  "MiningProxyUrl": null,
+  "MiningProxyCreds": null,
+  "BotListToken": null
+}
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
